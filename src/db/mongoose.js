7d82aeb2d1612f9fe582b0c93d260b0b72085e37
Alen Thankachan <alenthankz@gmail.com>
2020-07-01 10:25:54 -0700

    before deployment

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose =require('mongoose');
 //
-mongoose.connect('mongodb://127.0.0.1:27017/iot-api',{
+mongoose.connect(MONGODB_URL,{
     useNewUrlParser:true,
     useCreateIndex:true
 }).then((result)=>{
