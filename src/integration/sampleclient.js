7801b713ebd1fdc4924225af813bdbe0e4627e6e
Alen Thankachan <alenthankz@gmail.com>
2020-07-31 07:51:21 -0700

    dev changed

--- src/integration/sampleclient.js
+++ src/integration/sampleclient.js
@@ -29,7 +29,7 @@ client.on('connect', () => {
         batteryVoltage:Math.floor(Math.random() * 9.9)
       },
       {
-        id:"5f1195cc3655b21e88197c45",
+        id:"5f242f0fecc9ae0017a1aefa",
         status:Math.floor(Math.random() * 15)%5===0?'on':'off',
         temperature:Math.floor(Math.random() * 32),
         humidity:Math.floor(Math.random() * 24),
